15f2a783ef8817aecc0311030f75dcd2e78b17b6
Anton <pan300503@gmail.com>
2025-10-11 00:35:17 +0300

    added opengl first support

--- main.cpp
+++ main.cpp
@@ -1,13 +1,60 @@
 
-#include "actor.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <cstdint>
+
 #include "fmt/base.h"
 
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main() {
-  over::Actor actor;
+  fmt::println("Hello, opengl app!");
 
-  actor.Tick();
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  fmt::print("Hello, world!");
+  constexpr int32_t WIDTH = 1920;
+  constexpr int32_t HEIGHT = 1080;
+
+  GLFWwindow* window = glfwCreateWindow(
+      WIDTH, HEIGHT, "Hello, opengl, finally!", nullptr, nullptr);
+  if (!window) {
+    fmt::println("Failed to create glfw window");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    fmt::println("Failed to initialize GLAD");
+    return -1;
+  }
+
+  glViewport(0, 0, WIDTH, HEIGHT);
+  glfwSetFramebufferSizeCallback(window,
+                                 [](GLFWwindow* window, int width, int height) {
+                                   glViewport(0, 0, width, height);
+                                 });
+  glClearColor(0.2f, 0.3f, 0.3f, 1.f);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 
   return 0;
 }
