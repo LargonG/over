45c7c7a85e68b0337e547464f25c02ce15404cf6
Anton <pan300503@gmail.com>
2025-11-03 16:39:20 +0300

    structured project

--- basics/main.cpp
+++ basics/main.cpp
@@ -79,29 +79,15 @@ glm::mat4 LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 globalUp) {
   auto up = glm::normalize(glm::cross(backward, right));
 
   glm::mat4 result(glm::vec4(right, 0.0f), glm::vec4(up, 0.0f),
-                   glm::vec4(backward, 0.0f), glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+                   glm::vec4(backward, 0.0f),
+                   glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
   result = glm::transpose(result);
   result *= glm::translate(glm::mat4(1.0f), -position);
-  
+
   return result;
 }
 
 int main() {
-  // this code paragraph is for tutorial purposes
-  glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-  glm::mat4 trans = glm::mat4(1.0f);
-  trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 1.0f));
-  vec = trans * vec;
-  fmt::println("{}, {}, {}", vec.x, vec.y, vec.z);
-
-  // this too
-  trans = glm::mat4(1.0f);
-  trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-  trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
-  vec = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-  vec = trans * vec;
-  fmt::println("{}, {}, {}", vec.x, vec.y, vec.z);
-
   fmt::println("Hello, opengl app!");
 
   glfwInit();
@@ -127,72 +113,73 @@ int main() {
     return -1;
   }
 
+  // Set callbacks
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   glfwSetCursorPosCallback(window, cursorPositionUpdateCallback);
   glfwSetScrollCallback(window, scroolCallback);
+
+  int nAttrs;
+  glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nAttrs);
+  fmt::println("Maximum vertex attributes: {}", nAttrs);
+
+  // Game* Logic
 
   float vertices[] = {
       -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  // 0
       0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,  // 1
       0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  // 2
-      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  // 3
-      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  // 4
-      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  // 5
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  // 3
 
-      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  // 6
-      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  // 7
-      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // 8
-      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // 9
-      -0.5f, 0.5f,  0.5f,  0.0f, 1.0f,  // 10
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  // 4
+      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  // 5
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // 6
+      -0.5f, 0.5f,  0.5f,  0.0f, 1.0f,  // 7
+
+      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,  // 8
+      -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,  // 9
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  // 10
       -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  // 11
 
-      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,  // 12
-      -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,  // 13
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  // 14
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  // 15
-      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  // 16
-      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,  // 17
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // 12
+      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  // 13
+      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  // 14
+      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f,  // 15
 
-      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // 18
-      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  // 19
-      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  // 20
-      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  // 21
-      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f,  // 22
-      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // 23
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  // 16
+      0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,  // 17
+      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  // 18
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  // 19
 
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  // 24
-      0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,  // 25
-      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  // 26
-      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  // 27
-      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  // 28
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  // 29
-
-      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  // 30
-      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  // 31
-      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // 32
-      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // 33
-      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f,  // 34
-      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f   // 35
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  // 20
+      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  // 21
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // 22
+      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f,  // 23
   };
 
-  //uint32_t indices[] = {
-  //    0, 1, 3,  // 0
-  //    1, 2, 3,  // 1
-  //};
-
-  int nAttrs;
-  glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nAttrs);
-  fmt::println("maximum {}", nAttrs);
+  uint32_t indices[] = {
+      0,  1,  2,   // 0
+      2,  3,  0,   // 1
+      4,  5,  6,   // 2
+      6,  7,  4,   // 3
+      8,  9,  10,  // 4
+      10, 11, 8,   // 5
+      12, 13, 14,  // 6
+      14, 15, 12,  // 7
+      16, 17, 18,  // 8
+      18, 19, 16,  // 9
+      20, 21, 22,  // 10
+      22, 23, 20   // 11
+  };
 
   GLuint vao;
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  //GLuint ibo;
-  //glGenBuffers(1, &ibo);
-  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-  //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-  //             GL_STATIC_DRAW);
+  GLuint ibo;
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 
   GLuint vbo;
   glGenBuffers(1, &vbo);
@@ -226,25 +213,18 @@ int main() {
       glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
 
   glm::mat4 model;
-
-  /*glm::mat4 view = glm::mat4(1.0f);
-  view = glm::rotate(view, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-  view = glm::translate(view, glm::vec3(0.0f, -30.0f, -30.0f));*/
-
   glm::mat4 view;
   glm::mat4 proj;
 
   GLuint texture1 = loadTexture("textures/container.jpg");
   GLuint texture2 = loadTexture("textures/awesomeface.png", true);
 
-  over::Shader shader("vertex.shader", "fragment.shader");
+  over::Shader shader("shaders/vertex.shader", "shaders/fragment.shader");
   shader.Compile();
   shader.Activate();
 
   shader.SetInt("texture1", 0);
   shader.SetInt("texture2", 1);
-
-  shader.SetMatrix4f("transform", glm::value_ptr(trans));
 
   glViewport(0, 0, WIDTH, HEIGHT);
   glfwSetFramebufferSizeCallback(window,
@@ -252,8 +232,6 @@ int main() {
                                    glViewport(0, 0, width, height);
                                  });
   glClearColor(0.2f, 0.3f, 0.3f, 1.f);
-
-  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   glEnable(GL_DEPTH_TEST);
 
@@ -277,9 +255,6 @@ int main() {
 
     glBindVertexArray(vao);
 
-    /*view = glm::lookAt(camera.GetPosition(),
-                       camera.GetPosition() + camera.GetDirection(),
-                       camera.GetUp());*/
     view = LookAt(camera.GetPosition(),
                   camera.GetPosition() + camera.GetDirection(), camera.GetUp());
     shader.SetMatrix4f("view", glm::value_ptr(view));
@@ -292,28 +267,25 @@ int main() {
     for (std::size_t i = 0; i < 10; i++) {
       model = glm::mat4(1.0f);
       model = glm::translate(model, cubePositions[i]);
-      //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
       model = glm::rotate(model, timeValue * glm::radians(i * 20.0f),
                           glm::vec3(0.5f, 1.0f, 0.0f));
       shader.SetMatrix4f("model", glm::value_ptr(model));
 
-      trans = glm::mat4(1.0f);
+      auto trans = glm::mat4(1.0f);
       trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
       trans = glm::rotate(trans, timeValue, glm::vec3(0.0f, 0.0f, 1.0f));
 
       shader.SetMatrix4f("transform", glm::value_ptr(trans));
 
-      glDrawArrays(GL_TRIANGLES, 0, 36);
-      //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
+      glDrawElements(GL_TRIANGLES, 12 * 3, GL_UNSIGNED_INT, 0);
+      
       trans = glm::mat4(1.0f);
       trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
       trans = glm::scale(trans, glm::vec3(glm::abs(glm::sin(timeValue))));
 
       shader.SetMatrix4f("transform", glm::value_ptr(trans));
 
-      glDrawArrays(GL_TRIANGLES, 0, 36);
-      //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      glDrawElements(GL_TRIANGLES, 12 * 3, GL_UNSIGNED_INT, 0);
     }
 
     glBindVertexArray(0);
