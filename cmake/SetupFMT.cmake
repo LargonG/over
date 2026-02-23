b674282f2a6021e3260f35b3836c90ac240425a0
Anton <pan300503@gmail.com>
2026-02-23 18:34:45 +0300

    added include guards & moved includes into subdirectories

--- cmake/SetupFMT.cmake
+++ cmake/SetupFMT.cmake
@@ -1,6 +1,8 @@
+include_guard(GLOBAL)
 
 if (USE_VCPKG) 
 	find_package(fmt CONFIG REQUIRED)
 else()
+	message(STATUS "FMT CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
 	add_subdirectory(${CMAKE_SOURCE_DIR}/libs/fmt ${CMAKE_BINARY_DIR}/libs/fmt)
 endif()
